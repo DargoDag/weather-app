ed006f68c8c2930ba6a110da76fba55b2c887984
Magomed Magomedov <dargojs@gmail.com>
2023-07-10 14:53:07 +0300

    git init

---
+++ src/App.vue
@@ -0,0 +1,141 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { API_KEY, BASE_URL } from "./constants";
+import WeatherSummary from "./components/WeatherSummary.vue";
+import WeatherHighlight from "./components/WeatherHighlight.vue";
+import Coords from "./components/Coords.vue";
+import Humidity from "./components/Humidity.vue";
+
+const city = ref("Paris");
+const wheatherInfo = ref(null);
+
+function getWeather() {
+  fetch(`${BASE_URL}?q=${city.value}&units=metric&appid=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => (wheatherInfo.value = data));
+}
+onMounted(getWeather);
+</script>
+
+<template>
+  <div class="page">
+    <main class="main">
+      <div class="container">
+        <div class="laptop">
+          <div class="sections">
+            <section class="section section-left">
+              <div class="info">
+                <div class="city-inner">
+                  <input
+                    v-model="city"
+                    type="text"
+                    class="search"
+                    @keyup.enter="getWeather"
+                  />
+                </div>
+                <WeatherSummary :wheatherInfo="wheatherInfo" />
+              </div>
+            </section>
+            <section class="section section-right">
+              <WeatherHighlight :wheatherInfo="wheatherInfo" />
+            </section>
+          </div>
+          <div v-if="wheatherInfo?.weather" class="sections">
+            <Humidity :humidity="wheatherInfo.main.humidity" />
+            <Coords :coord="wheatherInfo.coord" />
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "./assets/styles/style.scss";
+.page {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  padding: 20px 0;
+  background-color: #b3a7e3;
+}
+.laptop {
+  width: 100%;
+  padding: 20px;
+  background-color: #0e100f;
+  border-radius: 25px;
+}
+.sections {
+  display: flex;
+  width: 100%;
+
+  @media (max-width: 767px) {
+    flex-direction: column;
+  }
+}
+.section-left {
+  width: 30%;
+  padding-right: 10px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    padding-right: 0;
+  }
+}
+.section-right {
+  width: 70%;
+  padding-left: 10px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    margin-top: 16px;
+    padding-left: 0;
+  }
+}
+.city-inner {
+  position: relative;
+  display: inline-block;
+  width: 100%;
+
+  &::after {
+    content: "";
+    position: absolute;
+    // top: 0;
+    right: 10px;
+    width: 25px;
+    height: 25px;
+    background: url("./assets/img/search.svg") no-repeat 50% 50%;
+    background-size: contain;
+    transform: translateY(50%);
+    cursor: pointer;
+  }
+}
+.info {
+  height: 100%;
+  padding: 16px;
+  background: url("./assets/img/gradient-1.jpg") no-repeat 50% 50%;
+  background-size: cover;
+  border-radius: 25px;
+}
+.search {
+  width: 100%;
+  padding: 16px;
+  font-family: "Inter", Arial, sans-serif;
+  color: $white;
+  background-color: rgba(0, 0, 0, 0.75);
+  border-radius: 16px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+.section-bottom {
+  width: 50%;
+  margin-top: 16px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const API_KEY = '4646bacefeae00ccf886f46aa0ba723e';
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
